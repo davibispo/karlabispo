31d20af9f74caa1c62247286ff5058cee90bdeb0
Davi <davi-bispo@hotmail.com>
2021-10-21 10:00:40 -0300

    rotas estabelecidas

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,38 @@
 <?php
 
+use App\Http\Controllers\{
+    RaizesController,
+    SiteController,
+    BlogController,
+    ClinicaController,
+    ContatoController,
+    LinkController,
+    LojaController,
+    PalestrasController,
+    VideosController,
+    CursosController,
+    PostController,
+};
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('posts', PostController::class);
+Route::resource('curso', CursosController::class);
+Route::resource('videos', VideosController::class);
+Route::resource('palestras', PalestrasController::class);
+Route::resource('loja', LojaController::class);
+Route::resource('links', LinkController::class);
+Route::resource('contato', ContatoController::class);
+Route::resource('clinica', ClinicaController::class);
+Route::resource('blog', BlogController::class);
+Route::resource('site', SiteController::class);
+Route::resource('raizes_da_infancia', RaizesController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 require __DIR__.'/auth.php';
