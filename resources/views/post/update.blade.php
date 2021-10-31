5aeb65ad016aac37fdea438715c742947bb46ea7
Davi <davi-bispo@hotmail.com>
2021-10-30 23:34:54 -0300

    ...

--- resources/views/post/update.blade.php
+++ resources/views/post/update.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-10">
             <div class="card">
-                <div class="card-header" style="font-weight:bold">Novo Artigo</div>
+                <div class="card-header" style="font-weight:bold">Editar Artigo</div>
 
                 <div class="card-body">
                     
